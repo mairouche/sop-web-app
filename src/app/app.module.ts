4be462ec9593e5e9cfe1fea62b70817e90b3140d
Meidi Airouche <meidi.airouche@caissedesdepots.fr>
2017-10-11 14:40:02 +0200

    Ajout du composant header dans un portail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { PortalComponent } from './portal/portal.component';
+import { PortalHeaderComponent } from './portal-header/portal-header.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'portal', pathMatch: 'full' },
+  { path: 'portal', component: PortalComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortalComponent,
+    PortalHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
